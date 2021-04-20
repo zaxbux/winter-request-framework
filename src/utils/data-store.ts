1156bb11651cff3416c36f84abe2e0f9eab78490
Zachary Schneider <hello@zacharyschneider.ca>
2021-04-19 22:03:34 -0700

    📝docs and typings

--- src/utils/data-store.ts
+++ src/utils/data-store.ts
@@ -1,55 +1,90 @@
 /* eslint-disable @typescript-eslint/ban-types */
 
-export class DOMDataStore<K extends object = object, V = any, T = any> {
-	private static instance: DOMDataStore;
-	private _storage: WeakMap<K, Map<V, T>>;
+/**
+ * Stores data associated to a object (usually a DOM Node), using a global WeakMap.
+ */
+export class DataStore<O extends object = object, K = any, V = any> {
+	private static instance: DataStore;
+	private _storage: WeakMap<O, Map<K, V>>;
 
 	constructor() {
-		if (!DOMDataStore.instance) {
-			this._storage = new WeakMap<K, Map<V, T>>();
-			DOMDataStore.instance = this;
+		if (!DataStore.instance) {
+			this._storage = new WeakMap<O, Map<K, V>>();
+			DataStore.instance = this;
 		}
 
-		return DOMDataStore.instance;
+		return DataStore.instance;
 	}
 
+	/**
+	 * Erase storage
+	 */
 	clear(): void {
-		this._storage = new WeakMap<K, Map<V, T>>();
+		this._storage = new WeakMap<O, Map<K, V>>();
 	}
 
-	remove(k: K, v: V): boolean {
-		if (!this._storage.has(k)) {
+	/**
+	 * Removes a key and value from storage.
+	 * 
+	 * @param o The object.
+	 * @param k The key.
+	 * @returns True if deleted.
+	 */
+	remove(o: O, k: K): boolean {
+		if (!this._storage.has(o)) {
 			return false;
 		}
 
-		const r = this._storage.get(k).delete(v);
+		const r = this._storage.get(o).delete(k);
 
-		if (this._storage.get(k).size === 0) {
-			this._storage.delete(k);
+		if (this._storage.get(o).size === 0) {
+			this._storage.delete(o);
 		}
 
 		return r;
 	}
 
-	get(k: K, v: V): T {
-		return this._storage.get(k).get(v);
+	/**
+	 * Gets a value for a Node from storage with the specified key.
+	 * 
+	 * @param o The object.
+	 * @param k The key.
+	 * @returns The value.
+	 */
+	get(o: O, k: K): V {
+		return this._storage.get(o).get(k);
 	}
 
-	has(k: K, v: V): boolean {
-		return this._storage.get(k) && this._storage.get(k).has(v);
+	/**
+	 * Checks if the object has an associated key.
+	 * 
+	 * @param o The object.
+	 * @param k The key.
+	 * @returns True if the value exists.
+	 */
+	has(o: O, k?: K): boolean {
+		return this._storage.get(o) && k && this._storage.get(o).has(k);
 	}
 
-	put(k: K, key: V, v: T): DOMDataStore<K> {
-		if (!this._storage.has(k)) {
-			this._storage.set(k, new Map<V, T>());
+	/**
+	 * Stores a value associated to an object with a key.
+	 * 
+	 * @param o The object.
+	 * @param k The key.
+	 * @param v The value.
+	 * @returns This instance.
+	 */
+	put(o: O, k: K, v: V): DataStore<O> {
+		if (!this._storage.has(o)) {
+			this._storage.set(o, new Map<K, V>());
 		}
 
-		this._storage.get(k).set(key, v);
+		this._storage.get(o).set(k, v);
 		return this;
 	}
 }
 
-const instance = new DOMDataStore<Node, string>();
+const instance = new DataStore<EventTarget, string>();
 Object.freeze(instance);
 
 export default instance;
